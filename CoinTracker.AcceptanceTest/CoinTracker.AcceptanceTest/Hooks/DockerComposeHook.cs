37b25ff63d0c8e4c1c9deec09448077e8f32daab
Giuseppe MAROLLA <gmarolla@talentia-software.com>
2024-08-17 16:46:26 +0200

    test

--- CoinTracker.AcceptanceTest/CoinTracker.AcceptanceTest/Hooks/DockerComposeHook.cs
+++ CoinTracker.AcceptanceTest/CoinTracker.AcceptanceTest/Hooks/DockerComposeHook.cs
@@ -27,7 +27,7 @@ internal static class DockerComposeHook
     {
         var composeFilePath = GetComposeFilePath();
         var containerBuilder = new Builder().UseContainer().UseCompose().FromFile(composeFilePath).RemoveOrphans().ForceBuild().Build().Start();
-        Thread.Sleep(60 * 1000);
+        Thread.Sleep(30 * 1000);
         testContext.Add(ContainerBuilder, containerBuilder);
     }
 
