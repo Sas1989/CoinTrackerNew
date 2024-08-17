bc5c44b727dbf66ff52b0e7b2a225cb6a15df502
Giuseppe MAROLLA <gmarolla@talentia-software.com>
2024-08-17 16:25:23 +0200

    test

--- CoinTracker.AcceptanceTest/CoinTracker.AcceptanceTest/Hooks/DatabaseRespawnHook.cs
+++ CoinTracker.AcceptanceTest/CoinTracker.AcceptanceTest/Hooks/DatabaseRespawnHook.cs
@@ -11,41 +11,4 @@ using System.Data.Common;
 
 namespace CoinTracker.AcceptanceTest.Hooks;
 
-[Binding]
-internal sealed class DatabaseRespawnHook 
-{
-    private readonly string[] SchemasList = new[] { "CoinList" };
-    private const string RespawnerKey = "respawner";
 
-    [BeforeScenario]
-    public async Task SetRespawnAsync(TestThreadContext testContext)
-    {
-        if (!testContext.ContainsKey(RespawnerKey))
-        {
-            var dbConnection = await GetOpenDbConnectionAsync(testContext);
-            var respawner = await Respawner.CreateAsync(dbConnection, new RespawnerOptions { SchemasToInclude = SchemasList });
-            testContext.Add("respawner", respawner);
-        }
-    }
-
-    [AfterScenario]
-    public async Task ResetDatabaseAsync(TestThreadContext testContext)
-    {
-        var dbConnection = await GetOpenDbConnectionAsync(testContext);
-
-        var respawner = testContext.Get<Respawner>(RespawnerKey);
-
-        await respawner.ResetAsync(dbConnection);
-    }
-
-
-    public async Task<DbConnection> GetOpenDbConnectionAsync(TestThreadContext testContext)
-    {
-        var dbConnection = testContext.Get<DbConnection>(TestContainerKeys.DbConnection);
-
-        if (dbConnection.State != ConnectionState.Open)
-            await dbConnection.OpenAsync();
-
-        return dbConnection;
-    }
-}
